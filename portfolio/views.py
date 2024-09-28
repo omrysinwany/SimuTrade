a7abe1dc133883a7c18e072ac2ac43e01d63a26e
Omry Sinwany <67701429+omrysinwany@users.noreply.github.com>
2024-09-28 20:57:25 +0300

    1

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Stock, Portfolio, Transaction
+from .serializers import StockSerializer, PortfolioSerializer, TransactionSerializer
 
-# Create your views here.
+class StockViewSet(viewsets.ModelViewSet):
+    queryset = Stock.objects.all()
+    serializer_class = StockSerializer
+
+class PortfolioViewSet(viewsets.ModelViewSet):
+    queryset = Portfolio.objects.all()
+    serializer_class = PortfolioSerializer
+
+class TransactionViewSet(viewsets.ModelViewSet):
+    queryset = Transaction.objects.all()
+    serializer_class = TransactionSerializer
